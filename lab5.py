adae3a969024af5d2af5012ab18a8795bb17c0ce
Hikatchus <76414030+Hikatchus@users.noreply.github.com>
2024-10-23 09:57:54 -0700

    Working on part 5

--- lab5.py
+++ lab5.py
@@ -1,5 +1,7 @@
-import data
+from typing import Optional
 
+import data
+from data import Time
 # Write your functions for each part in the space below.
 
 # Part 1
@@ -11,12 +13,29 @@ import data
 
 
 # Part 3
-
+def time_add(time1:Time, time2:Time) -> Time:
+    #initializes an output
+    output_time = Time(0,0,0)
+    #adds the seconds and take the remainder from 60, push the excess to carry
+    output_time.second = (time1.second + time2.second) % 60
+    carry_seconds = (time1.second + time2.second) // 60
+    #adds the minutes and take the remainder from 60, push the excess to carry
+    output_time.minute = (time1.minute + time2.minute + carry_seconds) % 60
+    carry_minutes = (time1.minute + time2.minute + carry_seconds) // 60
+    #adds the hours and carry
+    output_time.hour = time1.hour + time2.hour + carry_minutes
+    return output_time
 
 # Part 4
-
+def is_descending(input_list:list[float]) -> bool:
+    for n in range(len(input_list) - 1):
+        if input_list[n+1] >= input_list[n]:
+            return False
+    return True
 
 # Part 5
+def largest_between(input_list:list[int], lower:int, upper:int) -> Optional[int]:
+
 
 
 # Part 6
