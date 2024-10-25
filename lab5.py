7b3040cf5bf6d129dccf60c79449b02b9905e5c0
Hikatchus <76414030+Hikatchus@users.noreply.github.com>
2024-10-25 09:52:45 -0700

    All done!

--- lab5.py
+++ lab5.py
@@ -1,7 +1,7 @@
 from typing import Optional
-
+from math import sqrt
 import data
-from data import Time
+from data import Time, Point
 # Write your functions for each part in the space below.
 
 # Part 1
@@ -35,7 +35,36 @@ def is_descending(input_list:list[float]) -> bool:
 
 # Part 5
 def largest_between(input_list:list[int], lower:int, upper:int) -> Optional[int]:
+    largest = 0
+    if upper < lower:
+        return None
+    for n in range(lower,(upper+1)):
+        if input_list[n] > largest:
+            largest = input_list[n]
+    return largest
 
-
+list_a = [40, 10, 15, 5, 20, 35]
 
 # Part 6
+
+def distance(point_a: Point, point_b: Point) -> float:
+    length = abs(point_a.x - point_b.x)
+    height = abs(point_a.y - point_b.y)
+    return sqrt((length ** 2) +
+                (height ** 2))
+def furthest_from_origin(point_list:list[Point]) -> Optional[int]:
+    if point_list == []:
+        return None
+    distance_list = []
+    for n in point_list:
+        distance_list.append(distance(n, Point(0,0)))
+    highest_distance = 0
+    highest_idx = 0
+    for n in range(len(distance_list) -1):
+        if distance_list[n] > highest_distance:
+            highest_distance = distance_list[n]
+            highest_idx = n
+    return highest_idx
+
+
+list_b = [Point(4,2), Point(6, 10), Point(-2, 17), Point(-70, 80), Point(10, 19)]
--- lab5_tests.py
+++ lab5_tests.py
@@ -1,8 +1,8 @@
 import data
 import lab5
 import unittest
-from lab5 import time_add, is_descending
-from data import Time
+from lab5 import time_add, is_descending, largest_between, list_a, list_b, furthest_from_origin, distance
+from data import Time, Point
 
 
 # Write your test cases for each part below.
@@ -33,11 +33,21 @@ class TestCases(unittest.TestCase):
         test_list = [4.1, 7.2, 2.765, 1.0]
         self.assertEqual(is_descending(test_list), False)
     # Part 5
+    def test_largest_between_1(self):
+        list_a = [40, 10, 15, 5, 20, 35]
+        self.assertEqual(largest_between(list_a, 1, 4), 20)
 
+    def test_largest_between_2(self):
+        list_a = [40, 10, 15, 5, 20, 35]
+        self.assertEqual(largest_between(list_a, 0, 4), 40)
 
     # Part 6
-
-
+    def test_furthest_from_origin_1(self):
+        list_b = [Point(4,2), Point(6, 10), Point(-2, 17), Point(-70, 80), Point(10, 19)]
+        self.assertEqual(furthest_from_origin(list_b), 3)
+    def test_furthest_from_origin_2(self):
+        list_c = []
+        self.assertEqual(furthest_from_origin(list_c), None)
 
 
 
